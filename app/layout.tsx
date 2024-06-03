6a09ace3bc25026fc45494e545489fff2fc2daec
Heinry-Anselm <50972784+anselm-chilz@users.noreply.github.com>
2024-06-03 11:23:08 +0100

    Welcome Page Update

---
+++ app/layout.tsx
@@ -0,0 +1,20 @@
+import "./globals.css";
+
+import { Footer, NavBar } from "../components";
+
+export const metadata = {
+  title: "PetQuest",
+  description: "Amazing Pets waiting to be rehomed. Look through and adopt!",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang='en'>
+      <body className='relative'>
+        <NavBar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
---
+++ constants/index.ts
@@ -0,0 +1,97 @@
+export const breedsList = [
+  "Labrador Retriever",
+  "German Shepherd",
+  "Golden Retriever",
+  "Bulldog",
+  "Beagle",
+  "Poodle",
+  "Rottweiler",
+  "Yorkshire Terrier",
+  "Boxer",
+  "Dachshund",
+  "Great Dane",
+  "Siberian Husky",
+  "Doberman Pinscher",
+  "Australian Shepherd",
+  "Shih Tzu",
+  "Miniature Schnauzer",
+  "Chihuahua",
+  "Shetland Sheepdog",
+  "Boston Terrier",
+  "Pomeranian",
+  "Havanese",
+  "Mastiff",
+  "Brittany",
+  "English Springer Spaniel",
+  "Cocker Spaniel",
+  "Vizsla",
+  "Pug",
+  "Maltese",
+  "Cane Corso",
+  "Shiba Inu",
+];
+
+export const petAges = [
+  { title: "Age", value: "" },
+  { title: "Puppy/Kitten", value: "Puppy/Kitten" },
+  { title: "Young", value: "Young" },
+  { title: "Adult", value: "Adult" },
+  { title: "Senior", value: "Senior" },
+];
+
+export const petTypes = [
+  {
+    title: "Species",
+    value: "",
+  },
+  {
+    title: "Dog",
+    value: "Dog",
+  },
+  {
+    title: "Cat",
+    value: "Cat",
+  },
+  {
+    title: "Bird",
+    value: "Bird",
+  },
+  {
+    title: "Rabbit",
+    value: "Rabbit",
+  },
+  {
+    title: "Other",
+    value: "Other",
+  },
+];
+
+export const footerLinks = [
+  {
+    title: "About",
+    links: [
+      { title: "How it works", url: "/" },
+      { title: "Featured", url: "/" },
+      // { title: "Partnership", url: "/" },
+      // { title: "Business Relations", url: "/" },
+    ],
+  },
+  {
+    title: "Company",
+    links: [
+      { title: "Events", url: "/" },
+      { title: "Blog", url: "/" },
+      // { title: "Podcast", url: "/" },
+      // { title: "Invite a friend", url: "/" },
+    ],
+  },
+  {
+    title: "Socials",
+    links: [
+      { title: "Discord", url: "/" },
+      // { title: "Instagram", url: "/" },
+      // { title: "Twitter", url: "/" },
+      { title: "Facebook", url: "/" },
+    ],
+  },
+];
