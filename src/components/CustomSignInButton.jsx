5ca6c05846092b64fd4dadef303ef3210ab86eb0
doaaabulatifa <doaalatifa1@gmail.com>
2024-06-06 17:14:00 +0100

    buttons style

--- src/components/CustomSignInButton.jsx
+++ src/components/CustomSignInButton.jsx
@@ -3,7 +3,7 @@ import { SignInButton } from "@clerk/nextjs";
 const CustomSignInButton = () => {
   return (
     <SignInButton>
-      <button className="border bg-blue-600 hover:bg-blue-700 text-white rounded-full py-3 px-10 transition duration-300">
+      <button className="border bg-gradient-to-r from-blue-500 to-blue-700 hover:from-blue-600 hover:to-blue-800 text-white font-semibold rounded-full py-3 px-10 shadow-lg transform transition duration-300 hover:scale-105 focus:outline-none focus:ring-2 focus:ring-blue-300 focus:ring-opacity-50">
         Sign In
       </button>
     </SignInButton>
