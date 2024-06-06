091f1acf29b52848d9105c6f973fd6d37a803bc3
doaaabulatifa <doaalatifa1@gmail.com>
2024-06-06 18:02:37 +0100

    styling

--- src/components/CustomSignInButton.jsx
+++ src/components/CustomSignInButton.jsx
@@ -6,7 +6,7 @@ const CustomSignInButton = () => {
 
 
     <SignInButton>
-      <button className="m-8 text-2xl font-bold px-6 py-3 leading-none text-gray-200 border border-gray-800 rounded-lg focus:outline-none focus:shadow-outline bg-gradient-to-b from-gray-900 to-black hover:from-indigo-500 hover:to-blue-700 transition duration-300 ease-in-out transform hover:scale-105 shadow-lg">
+    <button className="smallmargintop text-2xl font-bold px-6 py-3 leading-none text-gray-200 border border-gray-800 rounded-lg focus:outline-none focus:shadow-outline bg-gradient-to-b from-gray-900 to-black hover:from-indigo-500 hover:to-blue-700 transition duration-300 ease-in-out transform hover:scale-105 shadow-lg">
         <span className="relative z-10">Sign In</span>
       </button>
     </SignInButton>
